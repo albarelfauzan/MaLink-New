cfba81a8537d22e0e7ed31651fb26e894a70d40c
Youvandra Febrial <youvandrafebrial@Youvandras-Mac-mini.local>
2024-09-27 15:38:46 +0700

    fix window

--- src/app/(DashboardLayout)/components/dashboard/contract.js
+++ src/app/(DashboardLayout)/components/dashboard/contract.js
@@ -1,10 +1,15 @@
-// contract.js
 import Web3 from "web3";
 import ABI from "./ABI.json";
 
-const web3 = new Web3(window.ethereum);
-const contractAddress = "0xc358A866f1E49698FD2b65c30f3dcfc42eAb337a"; // Replace with your contract address
+let web3;
+let contract;
 
-const contract = new web3.eth.Contract(ABI, contractAddress);
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+  web3 = new Web3(window.ethereum);
+  const contractAddress = "0xc358A866f1E49698FD2b65c30f3dcfc42eAb337a"; // Replace with your contract address
+  contract = new web3.eth.Contract(ABI, contractAddress);
+} else {
+  console.log('Ethereum wallet not detected. Please install MetaMask or another wallet.');
+}
 
 export { web3, contract };
