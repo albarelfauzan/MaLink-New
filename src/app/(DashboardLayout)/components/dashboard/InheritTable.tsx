bbb62162efdfc59836c1104d59b0c9e3998d690b
Youvandra Febrial <youvandrafebrial@Youvandras-Mac-mini.local>
2024-09-27 15:52:21 +0700

    fix web3 thing

--- src/app/(DashboardLayout)/components/dashboard/InheritTable.tsx
+++ src/app/(DashboardLayout)/components/dashboard/InheritTable.tsx
@@ -16,7 +16,7 @@ import {
 import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
 import AddGroupModal from '@/app/(DashboardLayout)/components/dashboard/AddGroup';
 import AddAccountModal from '@/app/(DashboardLayout)/components/dashboard/AddAccount';
-import DetailModal from '@/app/(DashboardLayout)/components/dashboard/DetailModal'; // Import modal baru
+import DetailModal from '@/app/(DashboardLayout)/components/dashboard/DetailModal'; 
 import { web3, contract } from './contract';
 import { useRouter } from 'next/navigation';
 
@@ -46,11 +46,10 @@ const InheritList = () => {
     const [openAddAccountModal, setOpenAddAccountModal] = useState(false);
     const [selectedGroupId, setSelectedGroupId] = useState<bigint | null>(null);
 
-
     const connectWallet = async () => {
         try {
             if (window.ethereum) {
-                const chainId = '0x405'; // Chain ID untuk BTTC Testnet
+                const chainId = '0x405'; 
                 const networkData = [{
                     chainId: chainId,
                     chainName: 'BTTC Testnet',
@@ -80,16 +79,21 @@ const InheritList = () => {
     };
 
     const fetchGroups = async (userAddress: string) => {
+        if (!web3) {
+            console.error('web3 is not initialized');
+            return;
+        }
+
         try {
             const response = await contract.methods.getAllGroups(userAddress).call();
             
             if (Array.isArray(response)) {
                 const updatedProducts = response.map((group: any) => ({
-                    id: BigInt(group.id), // Konversi id ke BigInt
+                    id: BigInt(group.id), 
                     name: group.name,
-                    totalAddresses: BigInt(group.totalAddresses), // Konversi totalAddresses ke BigInt
-                    totalAmount: web3.utils.fromWei(group.totalAmount, 'ether'), // Konversi amount ke format Ether
-                    releaseDate: BigInt(group.releaseDate), // Konversi releaseDate ke BigInt
+                    totalAddresses: BigInt(group.totalAddresses), 
+                    totalAmount: web3.utils.fromWei(group.totalAmount, 'ether'), 
+                    releaseDate: BigInt(group.releaseDate), 
                     isPaid: group.isPaid,
                     details: [],
                 }));
@@ -127,45 +131,35 @@ const InheritList = () => {
 
     const handleDetailOpen = async (groupId: bigint) => {
         console.log("Fetching recipients for group ID:", groupId.toString());
-        
-        // Reset selectedDetails to ensure it's empty before fetching new data
         setSelectedDetails([]);
-    
+
         try {
-            // Attempt to fetch the user's account, but allow modal to open regardless
             const accounts = await web3.eth.getAccounts();
             
-            // Use the provided groupId to fetch the recipients
             const response = await contract.methods.getGroupRecipients(Number(groupId)).call({
                 gas: "3000000",
-                from: accounts.length > 0 ? accounts[0] : undefined // Only use from if accounts exist
-            }) as [string[], string[]]; // Force type to tuple [string[], string[]]
-    
-            console.log("Response from contract:", response);
-    
-            const wallets = response[0];  // Access the first array
-            const amounts = response[1];  // Access the second array
-    
-            // Check if we have recipients
+                from: accounts.length > 0 ? accounts[0] : undefined 
+            }) as [string[], string[]];
+
+            const wallets = response[0];  
+            const amounts = response[1];  
+
             if (wallets.length === amounts.length && wallets.length > 0) {
                 const details = wallets.map((wallet: string, index: number) => ({
                     address: wallet,
-                    amount: web3.utils.fromWei(amounts[index], 'ether'), // Convert from wei to ether
+                    amount: web3.utils.fromWei(amounts[index], 'ether'), 
                 }));
-    
+
                 setSelectedDetails(details);
             } else {
-                
-                setSelectedDetails([]); // Set an empty array
+                setSelectedDetails([]); 
             }
         } catch (error) {
             console.error("Error fetching group recipients:", error);
         }
-    
-        // Open the detail modal regardless of the account status
+
         setOpenDetailModal(true);
     };
-    
 
     const handleAddAccountModalOpen = (groupId: bigint) => {
         setSelectedGroupId(groupId);
@@ -177,9 +171,7 @@ const InheritList = () => {
         setSelectedGroupId(null);
     };
     
-    
-
-    const filteredProducts = products.filter(product => {
+    const filteredProducts = products.filter((product: { isPaid: any; }) => {
         if (statusFilter === null) return true;
         return product.isPaid === statusFilter;
     });
@@ -187,7 +179,6 @@ const InheritList = () => {
     return (
         <DashboardCard
             title="Transaction List"
-            
             action={
                 <Button variant="outlined" disableElevation color="secondary" sx={{ borderRadius: 2 }} onClick={connectWallet}>
                     {wallet ? `Connected: ${wallet.slice(0, 6)}...${wallet.slice(-4)}` : "Connect Wallet"}
@@ -196,139 +187,151 @@ const InheritList = () => {
         >
             <>
             <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-    <Chip 
-        sx={{ bgcolor: '#F55C5C', color: '#fff', ml: -1, width: '425px', mb: 2 }} 
-        label={`In Testnet development, Transaction timing may be inaccurate.`} 
-    />
-</Box>
+                <Chip 
+                    sx={{ bgcolor: '#F55C5C', color: '#fff', ml: -1, width: '425px', mb: 2 }} 
+                    label={`In Testnet development, Transaction timing may be inaccurate.`} 
+                />
+            </Box>
 
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
-                    <Chip sx={{ bgcolor: '#01B574', color: '#fff', ml: -1 }} label={`${products.length} Group Created`} />
-                    <Button variant="contained" onClick={handleModalOpen} sx={{ bgcolor: "#AC6AEC", color: "#fff", padding: 1.5 }}>
-                        New Group
-                    </Button>
-                </Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
+                <Chip sx={{ bgcolor: '#01B574', color: '#fff', ml: -1 }} label={`${products.length} Group Created`} />
+                <Button variant="contained" onClick={handleModalOpen} sx={{ bgcolor: "#AC6AEC", color: "#fff", padding: 1.5 }}>
+                    New Group
+                </Button>
+            </Box>
 
-                <Box sx={{ display: 'flex', mt: 2 }}>
+            <Box sx={{ display: 'flex', mt: 2 }}>
                 <FormControl sx={{ minWidth: 200 }}>
-    <InputLabel sx={{ color: "#fff" }}>Status Filter</InputLabel>
-    <Select
-        value={statusFilter === null ? "All" : statusFilter ? "True" : "False"}
-        onChange={(e) => {
-            const value = e.target.value;
-            if (value === "All") {
-                setStatusFilter(null);
-            } else if (value === "True") {
-                setStatusFilter(true);
-            } else if (value === "False") {
-                setStatusFilter(false);
-            }
-        }}
-        label="Status Filter"
-        sx={{
-            color: "#fff",
-            '& .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'white'
-            },
-            '&:hover .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'white'
-            },
-            '& .MuiSvgIcon-root': {
-                color: '#fff',
-            },
-            '&.MuiSelect-root.MuiSelect-outlined': {
-                color: '#fff',
-            },
-        }}
-    >
-        <MenuItem value="All">All</MenuItem>
-        <MenuItem value="True">Released</MenuItem>
-        <MenuItem value="False">Upcoming</MenuItem>
-    </Select>
-</FormControl>
+                    <InputLabel sx={{ color: "#fff" }}>Status Filter</InputLabel>
+                    <Select
+                        value={statusFilter === null ? "All" : statusFilter ? "True" : "False"}
+                        onChange={(e: { target: { value: any; }; }) => {
+                            const value = e.target.value;
+                            if (value === "All") {
+                                setStatusFilter(null);
+                            } else if (value === "True") {
+                                setStatusFilter(true);
+                            } else if (value === "False") {
+                                setStatusFilter(false);
+                            }
+                        }}
+                        label="Status Filter"
+                        sx={{
+                            color: "#fff",
+                            '& .MuiOutlinedInput-notchedOutline': {
+                                borderColor: 'white'
+                            },
+                            '&:hover .MuiOutlinedInput-notchedOutline': {
+                                borderColor: 'white'
+                            },
+                            '& .MuiSvgIcon-root': {
+                                color: '#fff',
+                            },
+                            '&.MuiSelect-root.MuiSelect-outlined': {
+                                color: '#fff',
+                            },
+                        }}
+                    >
+                        <MenuItem value="All">All</MenuItem>
+                        <MenuItem value="True">Released</MenuItem>
+                        <MenuItem value="False">Upcoming</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
 
-                </Box>
-
-                <AddGroupModal open={openModal} onClose={handleModalClose} onAddGroup={handleAddGroup} />
-                <AddAccountModal
+            <AddGroupModal open={openModal} onClose={handleModalClose} onAddGroup={handleAddGroup} />
+            <AddAccountModal
                 open={openAddAccountModal}
                 onClose={handleAddAccountModalClose}
                 groupId={selectedGroupId}
-                fetchGroups={fetchGroups} // Pass function to refresh groups
+                fetchGroups={fetchGroups} 
             />
 
-<DetailModal
-                    open={openDetailModal}
-                    onClose={() => setOpenDetailModal(false)}
-                    details={selectedDetails}
-                />
+            <DetailModal
+                open={openDetailModal}
+                onClose={() => setOpenDetailModal(false)}
+                details={selectedDetails}
+            />
 
-                <Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
-                    <Table aria-label="simple table" sx={{ whiteSpace: "nowrap", mt: 2 }}>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell sx={{ color: "#A0AEC0" }}>No</TableCell>
-                                <TableCell sx={{ color: "#A0AEC0" }}>Name</TableCell>
-                                <TableCell sx={{ color: "#A0AEC0" }}>Account</TableCell>
-                                <TableCell sx={{ color: "#A0AEC0" }}>Amount</TableCell>
-                                <TableCell sx={{ color: "#A0AEC0" }}>Token</TableCell>
-                                <TableCell sx={{ color: "#A0AEC0" }}>Release Date</TableCell>
-                                <TableCell sx={{ color: "#A0AEC0" }}>Status</TableCell>
-                                <TableCell sx={{ color: "#A0AEC0" }}>Add Account</TableCell> {/* Kolom baru */}
-                                <TableCell sx={{ color: "#A0AEC0" }}>Details</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {filteredProducts.map((product, index) => (
-                                <TableRow key={product.name}>
-                                    <TableCell sx={{ color: "#fff" }}>{index + 1}</TableCell>
-                                    <TableCell sx={{ color: "#fff" }}>{product.name}</TableCell>
-                                    <TableCell sx={{ color: "#fff" }}>{product.totalAddresses.toString()} Account</TableCell>
-                                    <TableCell sx={{ color: "#fff" }}>{product.totalAmount}</TableCell>
-                                    <TableCell sx={{ color: "#fff" }}>USDT</TableCell>
-                                    <TableCell sx={{ color: "#fff" }}>{new Date(Number(product.releaseDate) * 1000).toLocaleString()}</TableCell>
-                                    <TableCell>
-                                        <Chip
-                                            sx={{
-                                                bgcolor: product.isPaid ? "#01B574" : "#FB9678",
-                                                color: "#fff",
-                                            }}
-                                            size="small"
-                                            label={product.isPaid ? "Released" : "Upcoming"}
-                                        />
-                                    </TableCell>
-                                    <TableCell>
+            <Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
+                <Table aria-label="simple table" sx={{ whiteSpace: "nowrap", mt: 2 }}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ color: "#A0AEC0" }}>No</TableCell>
+                            <TableCell sx={{ color: "#A0AEC0" }}>Name</TableCell>
+                            <TableCell sx={{ color: "#A0AEC0" }}>Release Date</TableCell>
+                            <TableCell sx={{ color: "#A0AEC0" }}>Total Addresses</TableCell>
+                            <TableCell sx={{ color: "#A0AEC0" }}>Total Amount</TableCell>
+                            <TableCell sx={{ color: "#A0AEC0" }}>Status</TableCell>
+                            <TableCell sx={{ color: "#A0AEC0" }}>Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {filteredProducts.map((product: { id: bigint; name: any; releaseDate: any; totalAddresses: { toString: () => any; }; totalAmount: any; isPaid: any; }, index: number) => (
+                            <TableRow key={product.id.toString()}>
+                                <TableCell>
+                                    <Typography sx={{ fontSize: "15px", fontWeight: "500" }}>
+                                        {index + 1}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography sx={{ fontSize: "15px", fontWeight: "500" }}>
+                                        {product.name}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography variant="h6" sx={{ fontWeight: "600" }}>
+                                        {new Date(Number(product.releaseDate) * 1000).toLocaleString()}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography variant="h6" sx={{ fontWeight: "600" }}>
+                                        {product.totalAddresses.toString()}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography variant="h6" sx={{ fontWeight: "600" }}>
+                                        {product.totalAmount} BTT
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Chip
+                                        sx={{
+                                            bgcolor: product.isPaid ? '#01B574' : 'error.main',
+                                            color: '#fff',
+                                            fontWeight: 'bold',
+                                        }}
+                                        size="small"
+                                        label={product.isPaid ? 'Released' : 'Upcoming'}
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <Box display="flex" alignItems="center">
                                         <Button
-                                            sx={{
-                                                color: "#01B574",
-                                                textTransform: "none",
-                                                fontWeight: "bold",
-                                            }}
-                                            onClick={() => handleAddAccountModalOpen(product.id)} 
+                                            sx={{ p: 1 }}
+                                            variant="contained"
+                                            color="secondary"
+                                            onClick={() => handleDetailOpen(product.id)}
+                                        >
+                                            Details
+                                        </Button>
+                                        <Button
+                                            sx={{ p: 1, ml: 1 }}
+                                            variant="contained"
+                                            color="secondary"
+                                            onClick={() => handleAddAccountModalOpen(product.id)}
                                         >
                                             Add Account
                                         </Button>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Button
-                                            sx={{
-                                                color: "#AC6AEC",
-                                                textTransform: "none",
-                                                fontWeight: "bold",
-                                            }}
-                                            onClick={() => handleDetailOpen(product.id)}
-                                        >
-                                            See Detail
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </Box>
+                                    </Box>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Box>
             </>
         </DashboardCard>
-
     );
 };
 
