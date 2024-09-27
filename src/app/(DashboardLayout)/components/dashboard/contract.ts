5f7c0357c257e319497df88002acc595319350be
albarelfauzan <eggavocados@gmail.com>
2024-09-27 18:44:16 +0700

    fix

--- src/app/(DashboardLayout)/components/dashboard/contract.ts
+++ src/app/(DashboardLayout)/components/dashboard/contract.ts
@@ -3,12 +3,12 @@ import ABI from "./ABI.json";
 
 // Tentukan tipe untuk web3 dan contract
 let web3: Web3 | undefined;
-let contract: Web3.eth.Contract | undefined; // This should be fine as long as you have the right import
+let contract: any; // Ubah menjadi any untuk menghindari error tipe
 
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.ethereum);
-    const contractAddress = "0xc358A866f1E49698FD2b65c30f3dcfc42eAb337a"; // Replace with your contract address
-    contract = new (web3 as any).eth.Contract(ABI, contractAddress); // Cast web3 to any to avoid type issues
+    const contractAddress = "0xc358A866f1E49698FD2b65c30f3dcfc42eAb337a"; // Ganti dengan alamat kontrak Anda
+    contract = new (web3 as any).eth.Contract(ABI, contractAddress); // Cast web3 ke any untuk menghindari masalah tipe
 } else {
     console.log('Ethereum wallet not detected. Please install MetaMask or another wallet.');
 }
