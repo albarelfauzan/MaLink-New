cf9ceffa0e0e9479183ae6510988561056a66b83
albarelfauzan <72002069+albarelfauzan@users.noreply.github.com>
2024-11-29 03:16:36 +0700

    Update contract.ts

--- src/app/(DashboardLayout)/components/dashboard/contract.ts
+++ src/app/(DashboardLayout)/components/dashboard/contract.ts
@@ -7,7 +7,7 @@ let contract: any; // Ubah menjadi any untuk menghindari error tipe
 
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.ethereum);
-    const contractAddress = "0xc358A866f1E49698FD2b65c30f3dcfc42eAb337a"; // Ganti dengan alamat kontrak Anda
+    const contractAddress = "0xd0702DE2cB79c4e5588107452a057D50E6DB82Bc"; // Ganti dengan alamat kontrak Anda
     contract = new (web3 as any).eth.Contract(ABI, contractAddress); // Cast web3 ke any untuk menghindari masalah tipe
 } else {
     console.log('Ethereum wallet not detected. Please install MetaMask or another wallet.');
